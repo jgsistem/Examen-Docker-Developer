d4103628dea80f1d07d1b322f137665e5fd54c1d
marvic <martinhuacho@hotmail.com>
2019-09-02 11:16:52 -0500

    agreganfo dockerfiles

---
+++ cineapp-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  //HOST: 'http://165.227.89.201/cineapp-backend',
+  // HOST: 'http://localhost:8099',
+  HOST: 'http://localhost',
+  PRECIO_ENTRADA: 'ENT',
+  REINTENTOS: 3,
+  TOKEN_AUTH_USERNAME: 'mitocineapp',
+  TOKEN_AUTH_PASSWORD: 'mito89codex',
+  TOKEN_NAME: 'access_token'
+};
